71a7daad349a092296160aba74949b24926f9faf
THARUN JAGADEESH <38012705+tharunjagadeesh@users.noreply.github.com>
2019-05-22 12:00:09 +0530

    Create main.c

---
+++ Bluetooth_USART/Bluetooth_USART/main.c
@@ -0,0 +1,64 @@
+/*
+ * Bluetooth_USART.c
+ *
+ * Created: 22-05-2019 11:59:42
+ * Author : Tharun Jagadeesh
+ */ 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <inttypes.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define LED PORTB
+char buffer[10];
+
+void USARTInit(uint16_t ubrr_value)
+{
+	
+	UBRRL = ubrr_value;
+	UBRRH = (ubrr_value>>8);
+	UCSRC=(1<<URSEL)|(1<<UCSZ0)| (1 << UCSZ1);
+	UCSRB=(1<<RXEN)|(1<<TXEN);
+}
+char USARTReadchar()
+{
+	
+	while(!(UCSRA & (1<<RXC)))
+	{
+	}
+	return UDR;
+}
+
+void USARTWritechar(char data)
+{
+	while(!(UCSRA & (1<<UDRE)))
+	{
+		
+	}
+	UDR=data;
+}
+int main()
+{
+	USARTInit(77);
+	DDRB=0xff;
+	char data;
+	LED = 0;
+	while(1)
+	{
+		
+		data=USARTReadchar();
+		if(data =='1')
+		{
+			LED |=0xff;
+		}
+		else if(data =='2')
+		{
+			LED =0x00;
+		}
+	}
+	return 0;
+}
